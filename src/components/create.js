2d1495205d7b08a1d2ca3595b72311ad4f8fc77d
GarrethWard <g00374946@gmit.ie>
2020-11-10 13:52:08 +0000

    lab complete

---
+++ src/components/create.js
@@ -0,0 +1,13 @@
+import React from 'react';
+import '../App.css';
+
+export class Create extends React.Component {
+
+  render() {
+    return (
+      <div className="App">
+        <h1>This is my Create</h1>
+      </div>
+    );
+  }
+}
